7b6827e4225b9058c06d94f1720296aca828a1fa
Dmitry Bolzan <dmitry.bolzan@gmail.com>
2019-10-11 02:51:29 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-module.exports = function zeros(expression) {
-  // your solution
+function zeros(expression) {
+  let array = expression.split(['*']);
+
+  let fiveCounter = 0;
+  let twoCounter = 0;
+
+  for (i = 0; i < array.length; i++) {
+    let factorial = array[i];
+
+    let number = parseInt(factorial, 10);
+
+    if (factorial[factorial.length - 2] == "!") {
+      if (number % 2 == 0) {
+        for (let j = 2; j <= number; j += 2) {
+          fiveCounter += incrementNumberCounter(j, 5)
+          twoCounter += incrementNumberCounter(j, 2)
+        }
+      } else {
+        for (let j = 1; j <= number; j += 2) {
+          fiveCounter += incrementNumberCounter(j, 5)
+        }
+      }
+    } else {
+      for (let j = 1; j <= number; j += 1) {
+        fiveCounter += incrementNumberCounter(j, 5)
+        twoCounter += incrementNumberCounter(j, 2)
+      }
+    }
+  }
+  return Math.min(twoCounter, fiveCounter);
 }
+
+function incrementNumberCounter(number, divider) {
+  let counter = 0;
+  if (number % divider == 0) {
+    while (number >= divider && number % divider == 0) {
+      counter++;
+      number /= divider;
+    }
+  }
+  return counter;
+}
+
+module.exports = zeros;
